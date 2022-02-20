a5c91399fa10184fdedc0b39147af5a0125fc92f
Yi-Ching Chen <jj97181818@gmail.com>
2022-02-21 05:24:50 +0800

    Implement q_insert_head function

--- queue.c
+++ queue.c
@@ -37,6 +37,24 @@ void q_free(struct list_head *l) {}
  */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+
+    element_t *node = malloc(sizeof(element_t));
+    if (!node) {
+        return false;
+    }
+
+    node->value = malloc((strlen(s) + 1) * sizeof(char));
+
+    if (!node->value) {
+        free(node);
+        return false;
+    }
+    strncpy(node->value, s, strlen(s) + 1);
+    list_add(&node->list, head);
+
     return true;
 }
 
